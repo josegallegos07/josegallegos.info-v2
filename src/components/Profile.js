f26c811a7f7c1afb4f7ae7fbf75aada6bd5d3084
Jose Gallegos <jose.gallegos@brainplasticity.com>
2020-06-27 22:47:13 -0700

    add homepage to package.json and fix relative url for gh-pages

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,7 +7,7 @@ import '../styles/components/Profile.scss';
 function Profile({ name, img, items }) {
   return (
     <div className="profile">
-      <img className="profile-img" src={`/img/${img}.jpg`} alt={name} />
+      <img className="profile-img" src={`img/${img}.jpg`} alt={name} />
       <div className="profile-name">{name}</div>
       {items.map((item, idx) => <ProfileItem key={`item-${idx}`} icon={item.icon} content={item.content} />)}
     </div>
